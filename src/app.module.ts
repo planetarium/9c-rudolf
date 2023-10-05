5ec961408b707c932cbc2b792c2d1890c6f7991f
XxshiftxX <qjfrntop12@gmail.com>
2023-10-05 11:17:18 +0900

    add transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { JobModule } from './job/job.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [PrismaModule, JobModule],
+  imports: [PrismaModule, JobModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
