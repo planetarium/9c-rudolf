c432c4de27b8dc0e9678223fc3fd753708d2def7
XxshiftxX <qjfrntop12@gmail.com>
2023-10-05 11:25:47 +0900

    add api: GET /transactions/:id

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TransactionService } from './transaction.service';
 
-@Controller('transaction')
-export class TransactionController {}
+@Controller('transactions')
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Get(':id')
+  async getTransaction(@Param('id') id: string) {
+    const transaction = await this.transactionService.getTransaction(id);
+
+    return {
+      actionType: transaction.jobs[0].actionType,
+      id: transaction.id,
+      status: transaction.status,
+      jobIds: transaction.jobs.map((job) => job.id),
+    };
+  }
+}
