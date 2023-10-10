18ffb16ab0b655db8b2b26f504b8923421d2ab88
moreal <dev.moreal@gmail.com>
2023-10-10 16:40:30 +0900

    Import TxModule

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { QueueController } from './queue.controller';
+import { TxModule } from 'src/tx/tx.module';
 
 @Module({
+  imports: [TxModule],
   providers: [QueueService, QueueController],
 })
 export class QueueModule {}
