ead616390ec3e1ef1021ba6fe1d36cf0f724ad3e
XxshiftxX <qjfrntop12@gmail.com>
2023-10-11 15:42:36 +0900

    move planetarium domin into TxService

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,30 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { BencodexDictionary } from '@planetarium/bencodex';
-import type { Currency } from '@planetarium/tx';
-import { ActionType, Job } from '@prisma/client';
-import { randomUUID } from 'crypto';
+import { ActionType } from '@prisma/client';
+
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TxService } from 'src/tx/tx.service';
-import esm_bypass_global from 'src/esm_bypass_global';
-
-const Address = esm_bypass_global['@planetarium/account'].Address;
-const encodeCurrency = esm_bypass_global['@planetarium/tx'].encodeCurrency;
-
-function encodeFungibleAssetValue(value) {
-  return [encodeCurrency(value.currency), value.rawValue];
-}
 
 const TX_ACTIONS_SIZE = 50;
-
-// FIXME: Get QUEUE_ADDRESS from external source (e.g., environment varibles).
-const QUEUE_ADDRESS = Address.fromHex(
-  '0x0000000000000000000000000000000000000000',
-);
-
-// FIXME: Get NCG_MINTER from external source (e.g., environment varibles).
-const NCG_MINTER = Address.fromHex(
-  '0x47D082a115c63E7b58B1532d20E631538eaFADde',
-);
 
 @Injectable()
 export class QueueService {
@@ -63,13 +43,9 @@ export class QueueService {
       const nextNonce = lastTx ? lastTx.nonce + 1n : 0n;
 
       // Create tx
-      const action =
-        actionType === ActionType.CLAIM_ITEMS
-          ? createClaimItemsAction(jobs)
-          : createTransferAssetsAction(jobs);
       const [id, { nonce }, raw] = await this.txService.createTx(
         nextNonce,
-        action,
+        jobs,
       );
       this.logger.debug(`[Job::${actionType}] tx created`, { id, nonce, raw });
 
@@ -82,81 +58,4 @@ export class QueueService {
       this.logger.debug(`[Job::${actionType}] tx processed`, { id, jobIds });
     });
   }
-}
-
-function createClaimItemsAction(jobs: Job[]) {
-  return new BencodexDictionary([
-    ['type_id', 'claim_items'],
-    [
-      'values',
-      new BencodexDictionary([
-        ['id', guidToBytes(randomUUID())],
-        [
-          'cd',
-          jobs.map((job) => [
-            Address.fromHex(job.address, true).toBytes(),
-            encodeFungibleAssetValue({
-              currency: {
-                ticker: job.ticker,
-                decimalPlaces: 18,
-                minters: new Set(),
-                totalSupplyTrackable: true,
-                maximumSupply: null,
-              },
-              rawValue: BigInt(Math.pow(job.amount, 18)),
-            }),
-          ]),
-        ],
-      ]),
-    ],
-  ]);
-}
-
-const CURRENCIES: Record<string, Currency> = {
-  NCG: {
-    ticker: 'NCG',
-    decimalPlaces: 2,
-    minters: new Set([NCG_MINTER.toBytes()]),
-    totalSupplyTrackable: false,
-    maximumSupply: null,
-  },
-  CRYSTAL: {
-    ticker: 'CRYSTAL',
-    decimalPlaces: 18,
-    minters: new Set(),
-    totalSupplyTrackable: false,
-    maximumSupply: null,
-  },
-};
-
-function createTransferAssetsAction(jobs: Job[]) {
-  return new BencodexDictionary([
-    ['type_id', 'transfer_assets'],
-    [
-      'values',
-      new BencodexDictionary([
-        ['sender', QUEUE_ADDRESS.toBytes()],
-        [
-          'recipients',
-          jobs.map((job) => [
-            Address.fromHex(job.address, true).toBytes(),
-            encodeFungibleAssetValue({
-              currency: CURRENCIES[job.ticker],
-              rawValue: BigInt(Math.pow(job.amount, 18)),
-            }),
-          ]),
-        ],
-      ]),
-    ],
-  ]);
-}
-
-function guidToBytes(guid) {
-  return new Uint8Array(
-    guid.split('-').reduce((bytes, section, index) => {
-      const bytesInChar = section.match(/.{1,2}/g);
-      if (index < 3) bytesInChar.reverse();
-      return bytes.concat(bytesInChar.map((byte) => parseInt(byte, 16)));
-    }, []),
-  );
 }
--- src/tx/tx.service.ts
+++ src/tx/tx.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import type { Account } from '@planetarium/account';
-import { BencodexDictionary, Value, encode } from '@planetarium/bencodex';
-import type { Currency } from '@planetarium/tx';
-import type { SignedTx, UnsignedTx } from '@planetarium/tx/dist/tx';
-import { createHash } from 'node:crypto';
-import esm_bypass_global from 'src/esm_bypass_global';
-import { Prisma, PrismaClient } from '@prisma/client';
+
+import { Job, Prisma, PrismaClient } from '@prisma/client';
 import { DefaultArgs } from '@prisma/client/runtime/library';
 
+import type { Account } from '@planetarium/account';
+import type { Currency } from '@planetarium/tx';
+import type { SignedTx, UnsignedTx } from '@planetarium/tx/dist/tx';
+import { BencodexDictionary, Value, encode } from '@planetarium/bencodex';
+
+import { createHash, randomUUID } from 'node:crypto';
+
+import esm_bypass_global from 'src/esm_bypass_global';
+
 const { Address, PublicKey } = esm_bypass_global['@planetarium/account'];
-const { encodeSignedTx, signTx } = esm_bypass_global['@planetarium/tx'];
 const { AwsKmsAccount, KMSClient } =
   esm_bypass_global['@planetarium/account-aws-kms'];
+const { encodeCurrency, encodeSignedTx, signTx } =
+  esm_bypass_global['@planetarium/tx'];
+
+// FIXME: Get QUEUE_ADDRESS from external source (e.g., environment varibles).
+const QUEUE_ADDRESS = Address.fromHex(
+  '0x0000000000000000000000000000000000000000',
+);
+
+// FIXME: Get NCG_MINTER from external source (e.g., environment varibles).
+const NCG_MINTER = Address.fromHex(
+  '0x47D082a115c63E7b58B1532d20E631538eaFADde',
+);
 
 const GENESIS_BLOCK_HASH = Buffer.from(
   '4582250d0da33b06779a8475d283d5dd210c683b9b999d74d03fac4f58fa6bce',
@@ -20,6 +35,22 @@ const GENESIS_BLOCK_HASH = Buffer.from(
 );
 const SUPER_FUTURE_DATETIME = new Date(2200, 12, 31, 23, 59, 59, 999);
 
+const CURRENCIES: Record<string, Currency> = {
+  NCG: {
+    ticker: 'NCG',
+    decimalPlaces: 2,
+    minters: new Set([NCG_MINTER.toBytes()]),
+    totalSupplyTrackable: false,
+    maximumSupply: null,
+  },
+  CRYSTAL: {
+    ticker: 'CRYSTAL',
+    decimalPlaces: 18,
+    minters: new Set(),
+    totalSupplyTrackable: false,
+    maximumSupply: null,
+  },
+};
 const MEAD_CURRENCY: Currency = {
   ticker: 'Mead',
   decimalPlaces: 18,
@@ -48,7 +79,81 @@ export class TxService {
     );
   }
 
-  async createTx(
+  public async createTx(nonce: bigint, jobs: Job[]) {
+    if (jobs.every((job) => job.actionType === jobs[0].actionType)) {
+      throw new Error('All jobs must have the same action type');
+    }
+
+    const actionType = jobs[0].actionType;
+    const action =
+      actionType === 'CLAIM_ITEMS'
+        ? this.buildClaimItemsAction(jobs)
+        : this.buildTransferAssetsAction(jobs);
+
+    return await this.createTxWithAction(nonce, action);
+  }
+
+  private guidToBytes(guid: ReturnType<typeof randomUUID>) {
+    return new Uint8Array(
+      guid.split('-').reduce((bytes, section, index) => {
+        const bytesInChar = section.match(/.{1,2}/g);
+        if (index < 3) bytesInChar.reverse();
+        return bytes.concat(bytesInChar.map((byte) => parseInt(byte, 16)));
+      }, []),
+    );
+  }
+
+  private buildTransferAssetsAction(jobs: Job[]) {
+    return new BencodexDictionary([
+      ['type_id', 'transfer_assets'],
+      [
+        'values',
+        new BencodexDictionary([
+          ['sender', QUEUE_ADDRESS.toBytes()],
+          [
+            'recipients',
+            jobs.map((job) => [
+              Address.fromHex(job.address, true).toBytes(),
+              this.encodeFungibleAssetValue({
+                currency: CURRENCIES[job.ticker],
+                rawValue: BigInt(Math.pow(job.amount, 18)),
+              }),
+            ]),
+          ],
+        ]),
+      ],
+    ]);
+  }
+
+  private buildClaimItemsAction(jobs: Job[]) {
+    return new BencodexDictionary([
+      ['type_id', 'claim_items'],
+      [
+        'values',
+        new BencodexDictionary([
+          ['id', this.guidToBytes(randomUUID())],
+          [
+            'cd',
+            jobs.map((job) => [
+              Address.fromHex(job.address, true).toBytes(),
+              this.encodeFungibleAssetValue({
+                currency: {
+                  ticker: job.ticker,
+                  decimalPlaces: 18,
+                  minters: new Set(),
+                  totalSupplyTrackable: true,
+                  maximumSupply: null,
+                },
+                rawValue: BigInt(Math.pow(job.amount, 18)),
+              }),
+            ]),
+          ],
+        ]),
+      ],
+    ]);
+  }
+
+  private async createTxWithAction(
     nonce: bigint,
     action: Value,
   ): Promise<[string, SignedTx<UnsignedTx>, Buffer]> {
@@ -109,5 +214,9 @@ export class TxService {
     }
 
     return 1n;
+  }
+
+  private encodeFungibleAssetValue(value) {
+    return [encodeCurrency(value.currency), value.rawValue];
   }
 }
