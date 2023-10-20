ded30723a084eb78b59bb819bf5ff7bb40f46e3f
XxshiftxX <qjfrntop12@gmail.com>
2023-10-20 10:22:16 +0900

    separate cron with controller

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
-import { QueueController } from './queue.controller';
+import { QueueCronController } from './queue.cron';
 import { TxModule } from 'src/tx/tx.module';
 
 @Module({
   imports: [TxModule],
-  providers: [QueueService, QueueController],
+  providers: [QueueService, QueueCronController],
 })
 export class QueueModule {}
