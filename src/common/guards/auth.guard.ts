25516359af4cd8bb9ae4ba7147ffad4e58e96b12
moreal <dev.moreal@gmail.com>
2023-11-02 12:52:11 +0900

    Implement auth guard with bearer token

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { RequireAuthToken } from '../decorators/required-auth-token.decorator';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly prismaSerivce: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    if (this.reflector.get(RequireAuthToken, context.getHandler())) {
+      const header = context
+        .switchToHttp()
+        .getRequest<Request>()
+        .header('Authorization');
+
+      if (!header) {
+        return false;
+      }
+
+      const splat = header.split(' ');
+      if (splat.length == 2 && splat[0] == 'Bearer') {
+        const authToken = await this.prismaSerivce.authToken.findUnique({
+          where: {
+            token: splat[1],
+          },
+        });
+
+        if (authToken?.token) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    return true;
+  }
+}
