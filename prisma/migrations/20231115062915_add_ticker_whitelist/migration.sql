1cd0c52f302f6c0b8e300b948144cf4e9b26b716
SHIFT <qjfrntop12@gmail.com>
2023-11-15 16:07:33 +0900

    Fix FAV__CRYSTAL tickeer
    Co-authored-by: Lee Dogeon <dev.moreal@gmail.com>

--- prisma/migrations/20231115062915_add_ticker_whitelist/migration.sql
+++ prisma/migrations/20231115062915_add_ticker_whitelist/migration.sql
@@ -10,4 +10,4 @@ CREATE TABLE "TickerWhitelist" (
 CREATE UNIQUE INDEX "TickerWhitelist_ticker_key" ON "TickerWhitelist"("ticker");
 
 INSERT INTO "TickerWhitelist"(ticker) VALUES
-('NCG'), ('FAV_CRYSTAL'), ('Item_NT_400000'), ('Item_NT_500000'), ('Item_T_40100015'), ('Item_T_40100016'), ('Item_T_40100017'), ('Item_T_10130002');
+('NCG'), ('FAV__CRYSTAL'), ('Item_NT_400000'), ('Item_NT_500000'), ('Item_T_40100015'), ('Item_T_40100016'), ('Item_T_40100017'), ('Item_T_10130002');
