39b381c05f0dd07fd2aeccd84ea64ec9e91c09ff
XxshiftxX <qjfrntop12@gmail.com>
2023-11-15 09:47:38 +0900

    add IsDefined validator on dto

--- src/job/dto/create-claim-items.dto.ts
+++ src/job/dto/create-claim-items.dto.ts
@@ -1,5 +1,6 @@
 import { Type } from 'class-transformer';
 import {
+  IsDefined,
   IsHexadecimal,
   IsInt,
   IsObject,
@@ -10,22 +11,27 @@ import {
 import { IsAvatarCurrency } from 'src/utils/currency';
 
 class Item {
+  @IsDefined()
   @IsAvatarCurrency()
   public ticker: string;
 
+  @IsDefined()
   @IsInt()
   public amount: number;
 }
 
 export class CreateClaimItemsDto {
+  @IsDefined()
   @IsString()
   public id: string;
 
+  @IsDefined()
   @IsString()
   @IsHexadecimal()
   @Length(42, 42)
   public avatarAddress: string;
 
+  @IsDefined()
   @IsObject()
   @ValidateNested()
   @Type(() => Item)
--- src/job/dto/create-transfer-assets.dto.ts
+++ src/job/dto/create-transfer-assets.dto.ts
@@ -7,6 +7,7 @@ import {
   ValidateNested,
   IsHexadecimal,
   Length,
+  IsDefined,
 } from 'class-validator';
 import {
   IsTicker,
@@ -15,29 +16,35 @@ import {
 } from 'src/utils/currency';
 
 class Item {
+  @IsDefined()
   @IsTicker()
   public ticker: string;
 
+  @IsDefined()
   @IsInt()
   public amount: number;
 }
 
 export class CreateTransferAssetsDto {
+  @IsDefined()
   @IsString()
   public id: string;
 
   @ValidateIf((o: CreateTransferAssetsDto) => isAvatarCurrency(o.item.ticker))
+  @IsDefined()
   @IsString()
   @IsHexadecimal()
   @Length(42, 42)
   public avatarAddress: string;
 
   @ValidateIf((o: CreateTransferAssetsDto) => isAgentCurrency(o.item.ticker))
+  @IsDefined()
   @IsString()
   @IsHexadecimal()
   @Length(42, 42)
   public agentAddress: string;
 
+  @IsDefined()
   @IsObject()
   @ValidateNested()
   @Type(() => Item)
