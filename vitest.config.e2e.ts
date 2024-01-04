ab426187beb88827d8cae714c93e1faa523bf43b
XxshiftxX <qjfrntop12@gmail.com>
2024-01-04 09:35:43 +0900

    add vite-tsconfig-paths

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,25 +1,18 @@
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
+    include: ['**/*.e2e-spec.ts'],
     exclude: ['k6/**/*', 'node_modules/**/*'],
     globals: true,
     root: './',
-    alias: {
-      src: './src',
-      test: './test',
-    },
-  },
-  resolve: {
-    alias: {
-      src: './src',
-      test: './test',
-    },
   },
   plugins: [
     swc.vite({
       module: { type: 'es6' },
     }),
+    tsconfigPaths(),
   ],
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,17 @@
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
     exclude: ['k6/**/*', 'node_modules/**/*'],
     globals: true,
     root: './',
-    alias: {
-      src: './src',
-      test: './test',
-    },
-  },
-  resolve: {
-    alias: {
-      src: './src',
-      test: './test',
-    },
   },
   plugins: [
     swc.vite({
       module: { type: 'es6' },
     }),
+    tsconfigPaths(),
   ],
 });
