5f61d28253cd47e2a2865874a011fbd1a1d0729a
moreal <dev.moreal@gmail.com>
2024-12-10 23:51:07 +0900

    style: apply `yarn lint`

--- k6/stress-test.spec.ts
+++ k6/stress-test.spec.ts
@@ -1,6 +1,6 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
 import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import { sleep } from 'k6';
+import http from 'k6/http';
 
 export const options = {
   vus: 100000,
